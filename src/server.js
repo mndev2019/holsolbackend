9d688a6fb14615c7a00b8bdcc87fae60afa8e5ee
mndev2019 <muskanvision2019@gmail.com>
2026-02-25 16:15:35 +0530

    `Removed commented out code and added server listening on '0.0.0.0'`

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const connectDB = require("./config/db");
-// const authRoutes = require("./routes/auth.routes");
+
 const mainroutes = require("./routes/routes")
 const path = require("path");
 
@@ -18,14 +18,24 @@ app.use(
 );
 
 // Routes
-// app.use("/api/auth", authRoutes);
+
 app.use(mainroutes)
+
 
 // DB Connection
 connectDB();
 
 // Start Server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+// Add '0.0.0.0' as the second argument
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
